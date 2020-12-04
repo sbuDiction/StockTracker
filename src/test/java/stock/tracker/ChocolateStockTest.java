e3303927bf08b7fc23017e32ff5774de405c7ae6
sbuDiction <nsbu68501@gmail.com>
2020-12-04 08:18:56 +0200

    deploying

---
+++ src/test/java/stock/tracker/ChocolateStockTest.java
@@ -0,0 +1,14 @@
+package stock.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChocolateStockTest {
+    @Test
+    public void greetUserNameTest() throws URISyntaxException {
+        assertEquals(0,0);
+    }
+}
